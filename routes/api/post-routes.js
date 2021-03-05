a93e96f1f5f9897d1df7beaa3ac6d5bea46650dc
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-05 11:47:04 -0500

    add route to update a post title

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -62,4 +62,30 @@ router.post('/', (req, res) => {
     });
 });
 
+// update a post title
+
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
