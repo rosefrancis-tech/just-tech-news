a4ace57ff801b681b949ed1490d95a8099d99d97
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-04 20:00:25 -0500

    verify user credentials for log in

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -53,6 +53,29 @@ router.post('/', (req, res) => {
       });
 });
 
+// login to user account
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 // update existing data
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
