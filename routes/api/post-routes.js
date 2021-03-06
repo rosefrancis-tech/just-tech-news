b7ebb4afb93e9f138641c3dc25d6f4fa751a07b7
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-06 10:25:59 -0500

    add comments to user

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,48 +1,62 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
 const sequelize = require('../../config/connection');
-
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id', 
-        'post_url', 
-        'title', 
-        'created_at', 
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      // show the latest news first
-      order: [['created_at', 'DESC']], 
-      include: [
-        {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// get one user post
 router.get('/:id', (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id
     },
     attributes: [
-      'id', 
-      'post_url', 
-      'title', 
+      'id',
+      'post_url',
+      'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -62,7 +76,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// create post
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -77,19 +90,15 @@ router.post('/', (req, res) => {
     });
 });
 
-// add a vote by updating post
-// PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
- // custom static method created in models/Post.js
- Post.upvote(req.body, { Vote })
- .then(updatedPostData => res.json(updatedPostData))
- .catch(err => {
-   console.log(err);
-   res.status(400).json(err);
- });
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
-// update a post title
 
 router.put('/:id', (req, res) => {
   Post.update(
@@ -115,7 +124,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// delete a post
 router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
@@ -134,7 +142,5 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
-
 
 module.exports = router;
