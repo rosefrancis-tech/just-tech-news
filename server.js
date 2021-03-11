14d4ea0716a9659ca93dbada6f48c81196ce5d82
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-11 12:00:00 -0500

    set up handlebars.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
